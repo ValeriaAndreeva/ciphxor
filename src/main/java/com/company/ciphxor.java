6df7d7f522525374749ebffe24c7e9729c03c99f
andreevavalerya <valeria.andreeva.2013@mail.ru>
2020-04-18 03:31:30 +0300

    xor

---
+++ src/main/java/com/company/ciphxor.java
@@ -0,0 +1,56 @@
+package com.company;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class ciphxor {
+    private static byte[] keyToByte(String key) {
+        String temp;
+        byte[] res = new byte[(int)Math.ceil((float)key.length() / 2)];
+        int pos = res.length - 1;
+        for (int i = key.length() - 1; i >= 0; i -= 2) {
+            if (i == 0)
+                temp = Character.toString(key.charAt(0));
+            else {
+                temp = key.substring(i - 1, i + 1);
+            }
+            res[pos] = (byte)Integer.parseInt(temp, 16);
+            pos--;
+        }
+        return res;
+    }
+
+    private static void recode(FileInputStream inputStream, String outFileName, String ckey, String dkey) throws IOException {
+            FileOutputStream out = new FileOutputStream(outFileName);
+            byte[] keyarr;
+
+            if (ckey != "" && dkey != "") {
+                throw new IOException("Задайте только один ключ");
+            }
+            if (ckey != "" || dkey != "") {
+                if (ckey != "") keyarr = keyToByte(ckey);
+                else keyarr = keyToByte(dkey);
+                byte temp;
+                byte res;
+                int i = 0;
+                while ((temp = (byte)inputStream.read()) != -1) {
+                    res = (byte)(temp^keyarr[i]);
+                    i++;
+                    i = i % keyarr.length;
+                    out.write(res);
+                }
+            } else {
+                throw new IOException("Ключа нет");
+            }
+            out.flush();
+            out.close();
+            inputStream.close();
+        }
+
+        public static void recode(String inputFileName, String outputFileName, String ckey, String dkey) throws IOException {
+        try (FileInputStream inputStream = new FileInputStream(inputFileName)) {
+            ciphxor.recode(inputStream, outputFileName, ckey, dkey);
+        }
+    }
+}
---
+++ src/main/java/launcher.java
@@ -0,0 +1,48 @@
+import com.company.ciphxor;
+import org.kohsuke.args4j.Argument;
+import org.kohsuke.args4j.CmdLineException;
+import org.kohsuke.args4j.CmdLineParser;
+import org.kohsuke.args4j.Option;
+
+import java.io.IOException;
+
+
+public class launcher {
+
+    @Option(name = "-c", metaVar = "Encoding", usage = "Input File encoding key")
+    private String Encoding = "";
+
+    @Option(name = "-d", metaVar = "Decoding", usage = "Input File decoding key")
+    private String Decoding = "";
+
+    @Option(name = "-o", metaVar = "OutputName", required = true, usage = "Output Name")
+    private String outputName;
+
+    @Argument(required = true, metaVar = "InputName", usage = "Input Name")
+    private String inputName;
+
+    public static void main(String[] args) {
+        new launcher().launch(args);
+    }
+
+    private void launch(String[] args) {
+        CmdLineParser parser = new CmdLineParser(this);
+        try {
+            parser.parseArgument(args);
+            if (outputName == null)
+                outputName = inputName + ".txt";
+        } catch (CmdLineException e) {
+            System.err.println(e.getMessage());
+            System.err.println("java -jar ciphxor.jar [-c Coding] [-d Decoding] InputName [-o OutputName]");
+            parser.printUsage(System.err);
+            return;
+        }
+        try {
+            ciphxor.recode(inputName, outputName, Encoding, Decoding);
+            System.out.println("");
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+}
+
